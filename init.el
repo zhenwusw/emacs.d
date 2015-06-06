4e98dde04ed581253f85796e67f3679fe27c5afd
zhen <ci@theplant.jp>
2015-06-06 17:52:24 +0800

    Config smex

--- init.el
+++ init.el
@@ -22,3 +22,8 @@
 (require 'ido)
 (ido-mode t)
 
+;;; Config Smex
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands) ;; this is your old M-x
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
