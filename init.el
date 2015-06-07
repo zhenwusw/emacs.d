1ef7c4ba3d18442261ae16c4c3e3c266aaf4222a
zhen <ci@theplant.jp>
2015-06-07 14:21:57 +0800

    Clean up init process

--- init.el
+++ init.el
@@ -1,45 +1,37 @@
+;;; How Packages work in Emacs 24
+;;; http://emacswiki.org/emacs/ELPA#toc4
+
 ;;; Package repositories
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-archives
-   (quote
-    (("marmalade" . "https://marmalade-repo.org/packages/")
-     ("melpa" . "http://melpa.org/packages/")
-     ("gnu" . "http://elpa.gnu.org/packages/")))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "https://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.org/packages/")))
 
-(package-initialize)
-
-;;; Config font
+;;; Basic configuration
 (set-frame-font "Monaco-16")
 (tool-bar-mode -1)
 
-;;; ido settings
-(require 'ido)
-(ido-mode t)
-
-;;; Load smartparens
-(smartparens-global-mode t)
-
-;;; Config Smex
-(global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "M-X") 'smex-major-mode-commands) ;; this is your old M-x
-(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
-
-;;; Config company
-(add-hook 'after-init-hook 'global-company-mode)
-
-;;; Enable yasnippet
+;;; ==================== non-ELPA packages =============
+;; Enable yasnippet
 (add-to-list 'load-path
              "~/.emacs.d/plugins/yasnippet")
 (require 'yasnippet)
 (yas-global-mode 1)
+
+;; basic initialization, (require) non-ELPA packages, etc.
+(setq package-enable-at-startup nil)
+(package-initialize)
+;; (require) your ELPA packages, configure them as normal
+
+;;; ==================== ELPA packages =============
+;; Config Smex
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands) ;; this is your old M-x
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
+;; After init hook
+(add-hook 'after-init-hook 'global-company-mode)
+(add-hook 'after-init-hook 'smartparens-global-mode)
+(add-hook 'after-init-hook 'smartparens-global-mode)
+(add-hook 'after-init-hook 'ido-mode)
+
 
