2a4e144c56c7d070735d1af1888ebbfce7161790
zhen <ci@theplant.jp>
2015-06-07 11:53:54 +0800

    Company, yet another auto completion

--- init.el
+++ init.el
@@ -28,3 +28,6 @@
 (global-set-key (kbd "M-X") 'smex-major-mode-commands) ;; this is your old M-x
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 
+;;; Config company
+(add-hook 'after-init-hook 'global-company-mode)
+
